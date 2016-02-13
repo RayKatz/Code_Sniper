926c3894cb6a0c3b443f5d86ffb8585eb3d884c3
RayKatz <saphirelynx@gmail.com>
2016-02-13 10:53:03 +0100

    Created PlayerBehaviour
    Added health to player
    Added LoseHealth()
    Added Die()

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public int health = 10;
+
+    // Use this for initialization
+
+    public void LoseHealth(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died.");
+        //Dying and stuff
+    }
+}
