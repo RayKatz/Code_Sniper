ec615cc1de39e5be416631e9cbd0ae97860dc3e0
RayKatz <saphirelynx@gmail.com>
2016-02-18 22:38:30 +0100

    Added screen transitions

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject[] canvasi;
+    public Animator fadeAnim;
+    int index;
+
+    void Start()
+    {
+        ChangeScreen(0);
+    }
+
+    public void ChangeScreen(int i)
+    {
+        index = i;
+
+        fadeAnim.SetTrigger("Change");
+        //fadeAnim.Play("FadeIn");
+
+        Invoke("Switch",1f);
+    }
+
+    void Switch()
+    {
+        foreach (GameObject c in canvasi)
+        {
+            c.SetActive(false);
+        }
+
+        canvasi[index].SetActive(true);
+    }
+
+}
