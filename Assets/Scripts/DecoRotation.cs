cfd6b24dd31f87e5e8e0aa9292c7ea714f1d8184
RayKatz <saphirelynx@gmail.com>
2016-02-18 14:11:03 +0100

    Added funky animation to title screen

---
+++ Assets/Scripts/DecoRotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecoRotation : MonoBehaviour {
+
+    public float intervall = 3;
+    Vector3 startRot;
+    Vector3 targetRot;
+    Hashtable hash;
+
+
+	void Start ()
+    {
+        InvokeRepeating("TurnIt", 0f, intervall);
+	}
+	
+	// Update is called once per frame
+	void TurnIt ()
+    {
+        hash = new Hashtable();
+        hash.Add("x", Random.Range(0f, 1f));
+        hash.Add("y", Random.Range(0f, 1f));
+        hash.Add("z", Random.Range(0f, 1f));
+        hash.Add("time", intervall);
+        hash.Add("easetype", iTween.EaseType.easeInOutCubic);
+
+        iTween.RotateBy(gameObject, hash);
+    }
+}
