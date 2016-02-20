a19ba8d24f61604d2ddd24ffc0bca71f6a869018
RayKatz <saphirelynx@gmail.com>
2016-02-20 15:07:40 +0100

    Added scene load on host server

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public string loadLevel;
+    string nextLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void SwitchLevel (string level)
+    {
+        //TODO: animate fader
+        GameObject.Find("Fade").GetComponent<Animator>().SetTrigger("Change");
+
+        nextLevel = level;
+        Invoke("Switch", 1f);
+	}
+
+    void Switch()
+    {
+        SceneManager.LoadScene(loadLevel);
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject.Find("Fade").GetComponent<Animator>().SetTrigger("Change");
+        yield return new WaitForSeconds(1.1f);
+        SceneManager.LoadScene(nextLevel);
+    }
+}
